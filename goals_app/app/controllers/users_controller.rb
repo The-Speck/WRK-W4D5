e99c7db0467d0f4d9e54f926152cd7a522c3fda9
Alex <a.plyushchay@gmail.com>
2018-06-15 17:23:22 -0400

    Add UserController actions

--- goals_app/app/controllers/users_controller.rb
+++ goals_app/app/controllers/users_controller.rb
@@ -1,7 +1,55 @@
 class UsersController < ApplicationController
   def index
     @user = User.all
-
     render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    # byebug
+    if user.save
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.try( :update, user_params )
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- goals_app/spec/controllers/users_controller_spec.rb
+++ goals_app/spec/controllers/users_controller_spec.rb
@@ -12,4 +12,75 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe 'GET #new' do
+    it 'renders the sign up page' do
+      get :new
+
+      expect(response).to render_template('new')
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with invalid params' do
+      it 'does not validate the presence of username and password' do
+        post :create, params: { user: { username: 'usernamesreas' } }
+        expect(response).to render_template('new')
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context 'with valid params' do
+      it 'validates the presence of username and password' do
+        post :create, params: { user: { username: 'usernamesreas', password: 'password123' }}
+        expect(response).to redirect_to(user_url(User.last.id))
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'renders the edit user page' do
+      get :edit, params: { id: user.id }
+
+      expect(response).to render_template('edit')
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders the all users page' do
+      get :show, params: { id: user.id }
+
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with invalid params' do
+      it 'does not validate the presence of username and password' do
+
+        put :update, params: { user: { username: 'usernamesreas', password: "" }, id: user.id  }
+        expect(response).to render_template('edit')
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context 'with valid params' do
+      it 'validates the presence of username and password' do
+        put :update, params: { user: { username: 'usernamesreas', password: 'password123' }, id: user.id }
+        expect(response).to redirect_to(user_url(User.last.id))
+      end
+    end
+  end
+
+
+  describe 'DELETE #destroy' do
+    it 'renders the all users page' do
+      get :destroy, params: { id: user.id }
+
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(200)
+    end
+  end
 end
